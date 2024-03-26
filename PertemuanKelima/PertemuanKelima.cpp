86530f47987aa0932d13aafb91cb155087eff56f
Asyurazhr <Asyuradjola2004@gmail.com>
2024-03-26 14:44:38 +0700

    add variabel global

--- PertemuanKelima/PertemuanKelima.cpp
+++ PertemuanKelima/PertemuanKelima.cpp
@@ -6,18 +6,6 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
-void inputData()
-{
-	cout << "Masukan bilangan pertama : ";
-	cin >> bilangan1;
-	cout << "Masukan bilangan kedua : ";
-	cin >> bilangan2;
-}
-
-int penjumlahan(int a, int b)
-{
-	return a + b;
-}
 
 
 int main()
